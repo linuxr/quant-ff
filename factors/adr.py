21927d7d68504f2f5433eb646c51ef0c2327c165
wangyubin <linuxr@aliyun.com>
2024-08-05 00:20:27 +0800

    add factor adtm

--- factors/adr.py
+++ factors/adr.py
@@ -1,1 +1,14 @@
 # -*- coding=utf-8 -*-
+
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class ADRFactor(Factor):
+    def signal(self, *args):
+        """
+        TODO: 上涨股票个数与下跌股票个数之比的简单移动平均
+        """
+        pass
--- factors/adtm.py
+++ factors/adtm.py
@@ -1,1 +1,59 @@
 # -*- coding=utf-8 -*-
+
+import common as cm
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class ADTMFactor(Factor):
+    def signal(self, *args):
+        """
+        通过比较开盘价往上涨的幅度和往下跌的幅度来衡量市场的人气
+        """
+        data = args[0]
+        n = args[1][0]
+        factor_name = args[2]
+
+        data["ref-open"] = cm.ref(data, "open", 1)
+        data["dtm"] = data.apply(
+            lambda z: (
+                max(
+                    z["high"] - z["open"],
+                    z["open"] - z["ref-open"],
+                )
+                if z["open"] > z["ref-open"]
+                else 0
+            ),
+            axis=1,
+        )
+        data["dbm"] = data.apply(
+            lambda z: (
+                max(
+                    z["open"] - z["low"],
+                    z["ref-open"] - z["open"],
+                )
+                if z["open"] < z["ref-open"]
+                else 0
+            ),
+            axis=1,
+        )
+        data["stm"] = cm.sum(data, "dtm", n)
+        data["sbm"] = cm.sum(data, "dbm", n)
+        data["max_stm_sbm"] = data.apply(lambda d: max(d["stm"], d["sbm"]), axis=1)
+
+        data[factor_name] = (data["stm"] - data["sbm"]) / data["max_stm_sbm"]
+
+        data = data.drop(
+            columns=[
+                "ref-open",
+                "dtm",
+                "dbm",
+                "stm",
+                "sbm",
+                "max_stm_sbm",
+            ]
+        )
+
+        return data
