63d636c74bb87366517558d46ffd9598beec630b
wangyubin <linuxr@aliyun.com>
2024-08-05 13:57:12 +0800

    add factor adx

--- factors/advpo.py
+++ factors/advpo.py
@@ -1,1 +1,15 @@
 # -*- coding=utf-8 -*-
+
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class ADVPOFactor(Factor):
+    def signal(self, *args):
+        """
+        TODO: 可以看成
+        ADVR 指标的变形，用来衡量（上涨股票成交量下跌股票成交量）占总成交量的比例
+        """
+        pass
--- factors/advr.py
+++ factors/advr.py
@@ -1,1 +1,14 @@
 # -*- coding=utf-8 -*-
+
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class ADVRFactor(Factor):
+    def signal(self, *args):
+        """
+        TODO: 上涨成交量的移动平均与下跌成交量的移动平均之比的移动平均
+        """
+        pass
