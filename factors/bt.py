4776f33d49ee39749f7812abedb86d369a4e6529
wangyubin <linuxr@aliyun.com>
2024-08-06 23:41:03 +0800

    add factor cci

--- factors/bt.py
+++ factors/bt.py
@@ -1,1 +1,17 @@
 # -*- coding=utf-8 -*-
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class BTFactor(Factor):
+    def signal(self, *args):
+        """
+        TODO: 用来衡量不同的移动平均价间的差距
+        """
+        # data = args[0]
+        # n = args[1][0]
+        # factor_name = args[2]
+
+        pass
--- factors/cci.py
+++ factors/cci.py
@@ -1,1 +1,27 @@
 # -*- coding=utf-8 -*-
+
+import common as cm
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class CCIFactor(Factor):
+    def signal(self, *args):
+        """
+        衡量典型价格（最高价、最低价和收盘价的均值）与其一段时间的移动平均的偏离程度
+        """
+        data = args[0]
+        n = args[1][0]
+        factor_name = args[2]
+
+        data["tp"] = (data["high"] + data["low"] + data["close"]) / 3
+        data["ma"] = cm.ma(data, "tp", n)
+        data["tp-ma"] = abs(data["tp"] - data["ma"])
+        data["md"] = cm.ma(data, "tp-ma", n)
+        data[factor_name] = (data["tp"] - data["ma"]) / (0.015 * data["md"])
+
+        data = data.drop(columns=["tp", "ma", "tp-ma", "md"])
+
+        return data
