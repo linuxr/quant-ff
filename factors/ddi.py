c1f9851cba484ced6748529eabe3d9c9c666862f
wangyubin <linuxr@aliyun.com>
2024-08-09 00:07:55 +0800

    add factor dema

--- factors/ddi.py
+++ factors/ddi.py
@@ -50,6 +50,7 @@ class DDIFactor(Factor):
                 "DMZ",
                 "DMF",
                 "DIZ",
+                "DIF",
             ]
         )
 
--- factors/dema.py
+++ factors/dema.py
@@ -1,1 +1,24 @@
 # -*- coding=utf-8 -*-
+
+import common as cm
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class DEMAFactor(Factor):
+    def signal(self, *args):
+        """
+        结合了单重 EMA 和双重 EMA，在保证平滑性的同时减少滞后性
+        """
+        data = args[0]
+        n = args[1][0]
+        factor_name = args[2]
+
+        data["EMA"] = cm.ema(data, N=n)
+        data[factor_name] = 2 * data["EMA"] - cm.ema(data, "EMA", n)
+
+        data = data.drop(columns=["EMA"])
+
+        return data
