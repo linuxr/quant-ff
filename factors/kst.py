942fee450c75c16896deb23ea7b762fb6e0361a4
wangyubin <linuxr@aliyun.com>
2024-08-12 14:37:58 +0800

    add factor kst

--- factors/kst.py
+++ factors/kst.py
@@ -1,1 +1,57 @@
 # -*- coding=utf-8 -*-
+
+import common as cm
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class KSTFactor(Factor):
+    def signal(self, *args):
+        """
+        结合了不同时间长度的 ROC 指标
+        """
+        data = args[0]
+        # n = args[1][0]
+        factor_name = args[2]
+
+        data["ref-close10"] = cm.ref(data, N=10)
+        data["roc-ma1"] = data["close"] - data["ref-close10"]
+        data["roc-ma1"] = cm.ma(data, "roc-ma1", 10)
+
+        data["ref-close15"] = cm.ref(data, N=15)
+        data["roc-ma2"] = data["close"] - data["ref-close15"]
+        data["roc-ma2"] = cm.ma(data, "roc-ma2", 10)
+
+        data["ref-close20"] = cm.ref(data, N=20)
+        data["roc-ma3"] = data["close"] - data["ref-close20"]
+        data["roc-ma3"] = cm.ma(data, "roc-ma3", 10)
+
+        data["ref-close30"] = cm.ref(data, N=30)
+        data["roc-ma4"] = data["close"] - data["ref-close30"]
+        data["roc-ma4"] = cm.ma(data, "roc-ma4", 10)
+
+        data["kst-ind"] = (
+            data["roc-ma1"]
+            + data["roc-ma2"] * 2
+            + data["roc-ma3"] * 3
+            + data["roc-ma4"] * 4
+        )
+        data[factor_name] = cm.ma(data, "kst-ind", 9)
+
+        data = data.drop(
+            columns=[
+                "ref-close10",
+                "ref-close15",
+                "ref-close20",
+                "ref-close30",
+                "roc-ma1",
+                "roc-ma2",
+                "roc-ma3",
+                "roc-ma4",
+                "kst-ind",
+            ]
+        )
+
+        return data
