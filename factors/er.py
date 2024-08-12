1d27545bc548f8e7ce2607212d86a0041f8816cc
wangyubin <linuxr@aliyun.com>
2024-08-12 13:03:22 +0800

    add factor er and expma

--- factors/er.py
+++ factors/er.py
@@ -1,1 +1,25 @@
 # -*- coding=utf-8 -*-
+
+import common as cm
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class ERFactor(Factor):
+    def signal(self, *args):
+        """
+        动量指标。用来衡量市场的多空力量对比
+        """
+        data = args[0]
+        n = args[1][0]
+        # factor_name = args[2]
+
+        data["ema-close"] = cm.ema(data, N=n)
+        data["BullPower"] = data["high"] - data["ema-close"]
+        data["BearPower"] = data["low"] - data["ema-close"]
+
+        # data = data.drop(columns=["mac"])
+
+        return data
--- factors/expma.py
+++ factors/expma.py
@@ -1,1 +1,23 @@
 # -*- coding=utf-8 -*-
+
+import common as cm
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class EXPMAFactor(Factor):
+    def signal(self, *args):
+        """
+        是简单移动平均的改进版，用于改善简单移动平均的滞后性问题
+        """
+        data = args[0]
+        n1 = args[1][0]
+        n2 = args[1][1]
+        # factor_name = args[2]
+
+        data["EMA1"] = cm.ema(data, N=n1)
+        data["EMA2"] = cm.ema(data, N=n2)
+
+        return data
