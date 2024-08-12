6c818c48da4b8e74f611c66ee7951bdb9cc576e5
wangyubin <linuxr@aliyun.com>
2024-08-12 13:13:47 +0800

    add factor hma

--- factors/hma.py
+++ factors/hma.py
@@ -1,1 +1,21 @@
 # -*- coding=utf-8 -*-
+
+import common as cm
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class HMAFactor(Factor):
+    def signal(self, *args):
+        """
+        简单移动平均线把收盘价替换为最高价
+        """
+        data = args[0]
+        n = args[1][0]
+        factor_name = args[2]
+
+        data[factor_name] = cm.ma(data, "high", n)
+
+        return data
