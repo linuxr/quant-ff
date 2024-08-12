8e1c7c1f097db21aa05ba8be16fda28b4d8b63e1
wangyubin <linuxr@aliyun.com>
2024-08-12 14:29:58 +0800

    add factor kc

--- factors/kc.py
+++ factors/kc.py
@@ -1,1 +1,37 @@
 # -*- coding=utf-8 -*-
+
+import common as cm
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class KCFactor(Factor):
+    def signal(self, *args):
+        """
+        与布林带类似，都是用价格的移动平均构造中轨，
+        不同的是表示波幅的方法，这里用 ATR 来作为波幅构造上下轨
+        """
+        data = args[0]
+        n = args[1][0]
+        # factor_name = args[2]
+
+        data["ref-close"] = cm.ref(data, N=1)
+        data["ref-low"] = cm.ref(data, "low", N=1)
+        data["tr"] = data.apply(
+            lambda z: max(
+                abs(z["high"] - z["low"]),
+                abs(z["high"] - z["ref-close"]),
+                abs(z["ref-close"] - z["ref-low"]),
+            ),
+            axis=1,
+        )
+        data["atr"] = cm.ma(data, "tr", n)
+        data["middle"] = cm.ema(data, N=20)
+        data["UPPER"] = data["middle"] + data["atr"] * 2
+        data["LOWER"] = data["middle"] - data["atr"] * 2
+
+        data = data.drop(columns=["ref-close", "ref-low", "tr", "atr", "middle"])
+
+        return data
