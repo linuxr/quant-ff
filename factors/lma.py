754f13e1c7c5ead4050af7c1e0688a7eae6f9a17
wangyubin <linuxr@aliyun.com>
2024-08-12 14:39:09 +0800

    add factor lma

--- factors/lma.py
+++ factors/lma.py
@@ -1,1 +1,21 @@
 # -*- coding=utf-8 -*-
+
+import common as cm
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class LMAFactor(Factor):
+    def signal(self, *args):
+        """
+        简单移动平均线把收盘价替换为最低价
+        """
+        data = args[0]
+        n = args[1][0]
+        factor_name = args[2]
+
+        data[factor_name] = cm.ma(data, "low", n)
+
+        return data
