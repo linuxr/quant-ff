9c534bec2aebb3b9bfb05a3ad4bef865f025d451
wangyubin <linuxr@aliyun.com>
2024-08-13 23:22:47 +0800

    update factor signal method bop bt

--- factors/bop.py
+++ factors/bop.py
@@ -1,6 +1,7 @@
 # -*- coding=utf-8 -*-
 
 import common as cm
+import pandas as pd
 
 from factors import Factor
 from dataclasses import dataclass
@@ -8,16 +9,14 @@ from dataclasses import dataclass
 
 @dataclass
 class BOPFactor(Factor):
-    def signal(self, *args):
+    def signal(self, data: pd.DataFrame, para: list):
         """
         衡量收盘价与开盘价的距离（正、负距离）占最高价与最低价的距离的比例
         """
-        data = args[0]
-        n = args[1][0]
-        factor_name = args[2]
+        n = para[0]
 
         data["tmp"] = (data["close"] - data["open"]) / (data["high"] - data["low"])
-        data[factor_name] = cm.ma(data, "tmp", n)
+        data[self.name] = cm.ma(data, "tmp", n)
 
         data = data.drop(columns=["tmp"])
 
--- factors/bt.py
+++ factors/bt.py
@@ -1,12 +1,13 @@
 # -*- coding=utf-8 -*-
 
+import pandas as pd
 from factors import Factor
 from dataclasses import dataclass
 
 
 @dataclass
 class BTFactor(Factor):
-    def signal(self, *args):
+    def signal(self, data: pd.DataFrame, para: list):
         """
         TODO: 用来衡量不同的移动平均价间的差距
         """
