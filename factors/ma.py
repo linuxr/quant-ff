c498976dd8a312be8ff4951806ac5f2d27d44f76
wangyubin <linuxr@aliyun.com>
2024-08-19 00:14:15 +0800

    add factor ma

--- factors/ma.py
+++ factors/ma.py
@@ -1,1 +1,20 @@
 # -*- coding=utf-8 -*-
+
+import common as cm
+import pandas as pd
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class MAFactor(Factor):
+    def signal(self, data: pd.DataFrame, para: list):
+        """
+        具有趋势的特性，它比较平稳，不像日K线会起起落落地震荡
+        """
+        n = para[0]
+
+        data[self.name] = cm.ma(data, N=n)
+
+        return data
