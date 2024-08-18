06929cc8c726925995e6ea1db2c86ca86b0e4f54
wangyubin <linuxr@aliyun.com>
2024-08-19 00:21:48 +0800

    add factor madisplaced

--- factors/madisplaced.py
+++ factors/madisplaced.py
@@ -1,1 +1,24 @@
 # -*- coding=utf-8 -*-
+
+import common as cm
+import pandas as pd
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class MADisplacedFactor(Factor):
+    def signal(self, data: pd.DataFrame, para: list):
+        """
+        把简单移动平均线向前移动了 M 个交易日，用法与一般的移动平均线一样
+        """
+        n = para[0]
+        m = para[1]
+
+        data["ma-close"] = cm.ma(data, N=n)
+        data[self.name] = cm.ref(data, "ma-close", m)
+
+        data = data.drop(columns=["ma-close"])
+
+        return data
