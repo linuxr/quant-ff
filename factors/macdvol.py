8fb963104b36bfc04eee72a861687b25e8e4c782
wangyubin <linuxr@aliyun.com>
2024-08-19 00:20:32 +0800

    add factor macdvol

--- factors/macdvol.py
+++ factors/macdvol.py
@@ -1,1 +1,22 @@
 # -*- coding=utf-8 -*-
+
+import common as cm
+import pandas as pd
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class MACDVOLFactor(Factor):
+    def signal(self, data: pd.DataFrame, para: list):
+        """
+        MACD 的成交量版本
+        """
+        n1 = para[0]
+        n2 = para[1]
+
+        data[self.name] = cm.ema(data, "volume", N=n1) - cm.ema(data, "volume", N=n2)
+        # data["SIGNAL"] = cm.ema(data, "MACDVOL", N3)
+
+        return data
