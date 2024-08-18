09030bfa69c518b2629340b49ff82fe2cd9b1619
wangyubin <linuxr@aliyun.com>
2024-08-19 00:37:33 +0800

    add factor pac

--- factors/pac.py
+++ factors/pac.py
@@ -1,1 +1,22 @@
 # -*- coding=utf-8 -*-
+
+import common as cm
+import pandas as pd
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class PACFactor(Factor):
+    def signal(self, data: pd.DataFrame, para: list):
+        """
+        用最高价和最低价的移动平均来构造价格变化的通道
+        """
+        n1 = para[0]
+        n2 = para[1]
+
+        data["UPPER"] = cm.sma(data, "high", n1, 1)
+        data["LOWER"] = cm.sma(data, "low", n2, 1)
+
+        return data
