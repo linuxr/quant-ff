c66b0bf8d72035e79c4d3044362e013a6bb1df60
wangyubin <linuxr@aliyun.com>
2024-08-19 00:17:35 +0800

    add factor maamt

--- factors/maamt.py
+++ factors/maamt.py
@@ -1,1 +1,20 @@
 # -*- coding=utf-8 -*-
+
+import common as cm
+import pandas as pd
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class MAAMTFactor(Factor):
+    def signal(self, data: pd.DataFrame, para: list):
+        """
+        成交额的移动平均线
+        """
+        n = para[0]
+
+        data[self.name] = cm.ma(data, "quote_volume", n)
+
+        return data
