83b0370250451e62e9c92603685c90201f881835
wangyubin <linuxr@aliyun.com>
2024-08-19 00:40:40 +0800

    add factor po

--- factors/po.py
+++ factors/po.py
@@ -1,1 +1,25 @@
 # -*- coding=utf-8 -*-
+
+import common as cm
+import pandas as pd
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class POFactor(Factor):
+    def signal(self, data: pd.DataFrame, para: list):
+        """
+        短期均线与长期均线之间的变化率
+        """
+
+        data["ema-short"] = cm.ema(data, N=9)
+        data["ema-long"] = cm.ema(data, N=26)
+        data[self.name] = (
+            (data["ema-short"] - data["ema-long"]) / data["ema-long"] * 100
+        )
+
+        data = data.drop(columns=["ema-short", "ema-long"])
+
+        return data
