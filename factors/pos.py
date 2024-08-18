ea32a00b0331a840f1876a97aeb9cbd0fbb85ba6
wangyubin <linuxr@aliyun.com>
2024-08-19 00:42:17 +0800

    add factor pos

--- factors/pos.py
+++ factors/pos.py
@@ -1,1 +1,28 @@
 # -*- coding=utf-8 -*-
+
+import common as cm
+import pandas as pd
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class POSFactor(Factor):
+    def signal(self, data: pd.DataFrame, para: list):
+        """
+        衡量当前的 N 天收益率在过去 N 天的 N 天收益率最大值和最小值之间的位置
+        """
+        n = para[0]
+
+        data["ref-close"] = cm.ref(data, N=n)
+        data["price"] = (data["close"] - data["ref-close"]) / data["ref-close"]
+        data["price-min"] = cm.min(data, "price", n)
+        data["price-max"] = cm.max(data, "price", n)
+        data[self.name] = (data["price"] - data["price-min"]) / (
+            data["price-max"] - data["price-min"]
+        )
+
+        data = data.drop(columns=["ref-close", "price", "price-min", "price-max"])
+
+        return data
