b2e2b7c67e74778424d4c7e8f7d7b97e74ef72a6
wangyubin <linuxr@aliyun.com>
2024-08-19 00:30:56 +0800

    add factor nvi

--- factors/nvi.py
+++ factors/nvi.py
@@ -1,1 +1,35 @@
 # -*- coding=utf-8 -*-
+
+import common as cm
+import pandas as pd
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class NVIFactor(Factor):
+    def signal(self, data: pd.DataFrame, para: list):
+        """
+        均线的一种，相比于普通均线有着更低的延迟性
+        """
+        n = para[0]
+
+        data["ref-volume"] = cm.ref(data, "volume", 1)
+        data["ref-close"] = cm.ref(data, "close", 1)
+
+        data["nvi-inc"] = data.apply(
+            lambda z: (
+                1 + (z["close"] - z["ref-close"]) / z["close"]
+                if z["volume"] < z["ref-volume"]
+                else 1
+            ),
+            axis=1,
+        )
+        data.loc[0, "nvi-inc"] = 100
+        data[self.name] = cm.cumprod(data, "nvi-inc")
+        data[f"{self.name}-MA"] = cm.ma(data, "NVI", n)
+
+        data = data.drop(columns=["ref-volume", "ref-close", "nvi-inc"])
+
+        return data
