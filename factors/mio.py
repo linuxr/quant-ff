4fa0cf8b1ff19c4a62919462414c5a020ea0e1ed
wangyubin <linuxr@aliyun.com>
2024-08-19 00:28:59 +0800

    add factor mtm

--- factors/mio.py
+++ factors/mio.py
@@ -1,1 +1,16 @@
 # -*- coding=utf-8 -*-
+
+import pandas as pd
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class MIOFactor(Factor):
+    def signal(self, data: pd.DataFrame, para: list):
+        """
+        （上涨股票个数-下跌股票个数）与总股票个数之比的 MACD值
+        TODO:
+        """
+        pass
--- factors/mtm.py
+++ factors/mtm.py
@@ -1,1 +1,20 @@
 # -*- coding=utf-8 -*-
+
+import common as cm
+import pandas as pd
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class MTMFactor(Factor):
+    def signal(self, data: pd.DataFrame, para: list):
+        """
+        用当天价格与 N 天前价格的差值来衡量价格的动量
+        """
+        n = para[0]
+
+        data[self.name] = data["close"] - cm.ref(data, N=n)
+
+        return data
