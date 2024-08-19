25369ebe2fe64e4056b01745a40a8dffdec97835
wangyubin <linuxr@aliyun.com>
2024-08-19 10:08:30 +0800

    add factor wma

--- factors/wma.py
+++ factors/wma.py
@@ -1,1 +1,25 @@
 # -*- coding=utf-8 -*-
+
+import common as cm
+import pandas as pd
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class WMAFactor(Factor):
+    def signal(self, data: pd.DataFrame, para: list):
+        """
+        加权移动平均
+        """
+        n = para[0]
+
+        data[self.name] = n * data["close"]
+        for i in range(1, n):
+            data[self.name] = data[self.name] + (n - i) * cm.ref(data, N=i)
+
+        s = n * (n + 1) // 2
+        data[self.name] = data[self.name] / s
+
+        return data
