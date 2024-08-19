fc63e8882257b8398665b34ffbe607f477a71214
wangyubin <linuxr@aliyun.com>
2024-08-19 08:53:13 +0800

    add factor rmi

--- factors/reg.py
+++ factors/reg.py
@@ -1,1 +1,17 @@
 # -*- coding=utf-8 -*-
+
+# import common as cm
+import pandas as pd
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class REGFactor(Factor):
+    def signal(self, data: pd.DataFrame, para: list):
+        """
+        过去的 N 天内收盘价对序列[1,2,...,N]作回归得到回归直线
+        TODO: 用到机器学习，得到回归曲线
+        """
+        pass
--- factors/rmi.py
+++ factors/rmi.py
@@ -1,1 +1,31 @@
 # -*- coding=utf-8 -*-
+
+import common as cm
+import pandas as pd
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class RMIFactor(Factor):
+    def signal(self, data: pd.DataFrame, para: list):
+        """
+        与 RSI 的计算方式类似，
+        将 RSI 中的动量与前一天价格之差CLOSE-REF(CLOSE,1)项改为了与前四天价格之差 CLOSE-REF(CLOSE,4)
+        """
+        n = para[0]
+
+        data["sma1"] = cm.ref(data, N=4)
+        data["sma1"] = data.apply(
+            lambda z: max(z["close"] - z["sma1"], 0),
+            axis=1,
+        )
+
+        data["sma2"] = cm.ref(data, N=1)
+        data["sma2"] = abs(data["close"] - data["sma2"])
+        data[self.name] = cm.sma(data, "sma1", n, 1) / cm.sma(data, "sma2", n, 1) * 100
+
+        data = data.drop(columns=["sma1", "sma2"])
+
+        return data
