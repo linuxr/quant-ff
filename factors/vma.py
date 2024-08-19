4827b8b0d22ca6c1df516601698e6920ecbf45b5
wangyubin <linuxr@aliyun.com>
2024-08-19 09:53:58 +0800

    add factor vma

--- factors/vma.py
+++ factors/vma.py
@@ -1,1 +1,23 @@
 # -*- coding=utf-8 -*-
+
+import common as cm
+import pandas as pd
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class VMAFactor(Factor):
+    def signal(self, data: pd.DataFrame, para: list):
+        """
+        简单移动平均把收盘价替换为最高价、最低价、开盘价和收盘价的平均值
+        """
+        n = para[0]
+
+        data["PRICE"] = (data["high"] + data["low"] + data["open"] + data["close"]) / 4
+        data[self.name] = cm.ma(data, "PRICE", n)
+
+        data = data.drop(columns=["PRICE"])
+
+        return data
