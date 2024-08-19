0ab12494ed8596970666e7473d9afb66aa4d4779
wangyubin <linuxr@aliyun.com>
2024-08-19 09:58:34 +0800

    add factor vramt

--- factors/vramt.py
+++ factors/vramt.py
@@ -1,1 +1,51 @@
 # -*- coding=utf-8 -*-
+
+import common as cm
+import pandas as pd
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class VRAMTFactor(Factor):
+    def signal(self, data: pd.DataFrame, para: list):
+        """
+        与 VR 指标（Volume Ratio）一样，只是把其中的成交量替换成了成交额
+        """
+        n = para[0]
+
+        data["ref-close"] = cm.ref(data, N=1)
+        data["av"] = data.apply(
+            lambda z: z["quote_volume"] if z["close"] > z["ref-close"] else 0,
+            axis=1,
+        )
+        data["bv"] = data.apply(
+            lambda z: z["quote_volume"] if z["close"] < z["ref-close"] else 0,
+            axis=1,
+        )
+        data["cv"] = data.apply(
+            lambda z: z["quote_volume"] if z["close"] == z["ref-close"] else 0,
+            axis=1,
+        )
+
+        data["avs"] = cm.sum(data, "av", n)
+        data["bvs"] = cm.sum(data, "bv", n)
+        data["cvs"] = cm.sum(data, "cv", n)
+        data[self.name] = (data["avs"] + data["cvs"] / 2) / (
+            data["bvs"] + data["cvs"] / 2
+        )
+
+        data = data.drop(
+            columns=[
+                "ref-close",
+                "av",
+                "bv",
+                "cv",
+                "avs",
+                "bvs",
+                "cvs",
+            ]
+        )
+
+        return data
