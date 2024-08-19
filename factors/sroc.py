509617df858f0d51907668e68f90955e1d7036dd
wangyubin <linuxr@aliyun.com>
2024-08-19 09:23:22 +0800

    add factor sroc

--- factors/sroc.py
+++ factors/sroc.py
@@ -1,1 +1,25 @@
 # -*- coding=utf-8 -*-
+
+import common as cm
+import pandas as pd
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class SROCFactor(Factor):
+    def signal(self, data: pd.DataFrame, para: list):
+        """
+        衡衡量价格的涨跌幅
+        """
+        n = para[0]
+        m = para[1]
+
+        data["emap"] = cm.ema(data, N=n)
+        data["ref-emap"] = cm.ref(data, "emap", m)
+        data[self.name] = (data["emap"] - data["ref-emap"]) / data["ref-emap"]
+
+        data = data.drop(columns=["emap", "ref-emap"])
+
+        return data
