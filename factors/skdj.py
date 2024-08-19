890417e43ed7de3e9a4e44713f930a1a096a6be7
wangyubin <linuxr@aliyun.com>
2024-08-19 09:19:41 +0800

    add factor skdj

--- factors/skdj.py
+++ factors/skdj.py
@@ -1,1 +1,31 @@
 # -*- coding=utf-8 -*-
+
+import common as cm
+import pandas as pd
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class SKDJFactor(Factor):
+    def signal(self, data: pd.DataFrame, para: list):
+        """
+        慢速随机波动（即慢速 KDJ）
+        """
+        n = para[0]
+
+        data["min-low"] = cm.min(data, "low", n)
+        data["max-high"] = cm.max(data, "high", n)
+        data["RSV"] = (
+            (data["close"] - data["min-low"])
+            / (data["max-high"] - data["min-low"])
+            * 100
+        )
+        data["MARSV"] = cm.sma(data, "RSV", 3, 1)
+        data["K"] = cm.sma(data, "MARSV", 3, 1)
+        data["D"] = cm.ma(data, "K", 3)
+
+        data = data.drop(columns=["min-low", "max-high", "RSV", "MARSV"])
+
+        return data
