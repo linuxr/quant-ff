413389736ecdb8285b89f0641816682b43543133
wangyubin <linuxr@aliyun.com>
2024-08-19 09:38:42 +0800

    add factor trix

--- factors/trin.py
+++ factors/trin.py
@@ -1,1 +1,17 @@
 # -*- coding=utf-8 -*-
+
+# import common as cm
+import pandas as pd
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class TRINFactor(Factor):
+    def signal(self, data: pd.DataFrame, para: list):
+        """
+        综合考虑了上涨、下跌股票的个数和成交量
+        TODO:
+        """
+        pass
--- factors/trix.py
+++ factors/trix.py
@@ -1,1 +1,27 @@
 # -*- coding=utf-8 -*-
+
+import common as cm
+import pandas as pd
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class TRIXFactor(Factor):
+    def signal(self, data: pd.DataFrame, para: list):
+        """
+        价格的三重指数移动平均的变化率
+        """
+        n = para[0]
+
+        data["ema"] = cm.ema(data, N=n)
+        data["ema"] = cm.ema(data, "ema", n)
+        data["ema"] = cm.ema(data, "ema", n)
+
+        data["ref-ema"] = cm.ref(data, "ema", 1)
+        data[self.name] = (data["ema"] - data["ref-ema"]) / data["ref-ema"]
+
+        data = data.drop(columns=["ema", "ref-ema"])
+
+        return data
