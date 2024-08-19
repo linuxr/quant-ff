b9113f7678ca5eba7e773ff388a1e1a1f0dff919
wangyubin <linuxr@aliyun.com>
2024-08-19 10:00:03 +0800

    add factor vwap

--- factors/vwap.py
+++ factors/vwap.py
@@ -1,1 +1,26 @@
 # -*- coding=utf-8 -*-
+
+import common as cm
+import pandas as pd
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class VWAPFactor(Factor):
+    def signal(self, data: pd.DataFrame, para: list):
+        """
+        以成交量为权重计算价格的加权平均
+        """
+        n = para[0]
+
+        data["typical"] = (data["high"] + data["low"] + data["close"]) / 3
+        data["mf"] = data["volume"] * data["typical"]
+        data["vol-sum"] = cm.sum(data, "volume", n)
+        data["mf-sum"] = cm.sum(data, "mf", n)
+        data[self.name] = data["mf-sum"] / data["vol-sum"]
+
+        data = data.drop(columns=["typical", "mf", "vol-sum", "mf-sum"])
+
+        return data
