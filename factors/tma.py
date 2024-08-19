c247aea5af30e22028f090d2efc0a7e1a4c929d7
wangyubin <linuxr@aliyun.com>
2024-08-19 09:34:40 +0800

    add factor tma

--- factors/tma.py
+++ factors/tma.py
@@ -1,1 +1,23 @@
 # -*- coding=utf-8 -*-
+
+import common as cm
+import pandas as pd
+
+from factors import Factor
+from dataclasses import dataclass
+
+
+@dataclass
+class TMAFactor(Factor):
+    def signal(self, data: pd.DataFrame, para: list):
+        """
+        与其他的均线类似，不同的是，TMA 则赋予考虑的时间段内时间靠中间的价格更高的权重
+        """
+        n = para[0]
+
+        data["close-ma"] = cm.ma(data, N=n)
+        data[self.name] = cm.ma(data, "close-ma", n)
+
+        data = data.drop(columns=["close-ma"])
+
+        return data
